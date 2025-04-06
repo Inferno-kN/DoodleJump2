ef097cb959661ddae4092e5f86131555fd875987
Inferno-kN <aldosovich@bk.ru>
2025-04-07 00:09:59 +0300

    refactor: add rectangle in def __init__

--- Platform.py
+++ Platform.py
@@ -5,3 +5,10 @@ import settings
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('resources/images/platform.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (settings.PLATFORM_WIDTH, settings.PLATFORM_HEIGHT))
+        self.rectangle = self.image.get_rect()
+        self.rectangle.x = x
+        self.rectangle.y = y
+
+
