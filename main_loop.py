5b0e8627256ebe8298b9ff0862d3b5b75a08d57d
Inferno-kN <aldosovich@bk.ru>
2025-04-07 00:37:26 +0300

    refactor: rename import module platform

--- main_loop.py
+++ main_loop.py
@@ -3,7 +3,7 @@ import random
 import pygame
 
 from game_over import GameOver
-from Platform import Platform
+from platform import Platform
 from Player import Player
 from settings import WIDTH, HEIGHT, FPS, PLATFORM_WIDTH, clock, PLATFORM_HEIGHT, screen
 from background import Background
