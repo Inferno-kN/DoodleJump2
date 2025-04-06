436e093e98c9049a9b8136390f973d0636e5405b
Inferno-kN <aldosovich@bk.ru>
2025-04-06 23:59:41 +0300

    refactor: def draw()

--- GameOver.py
+++ GameOver.py
@@ -6,4 +6,8 @@ class GameOver:
         self.font = pygame.font.Font(None, 36)
         self.score = 0
 
-    def draw(self, surface, score): pass
+    def draw(self, surface, score):
+        text1 = self.font.render("Вы проиграли!", True, settings.WHITE)
+        text_rectangle1 = text1.get_rect(center=(settings.WIDTH / 2, settings.HEIGHT / 2 - 20))
+        surface.blit(text1, text_rectangle1)
+
