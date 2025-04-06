ab834cab4b4a0c355df9ebca580cc94ce7e733d9
Inferno-kN <aldosovich@bk.ru>
2025-04-07 00:21:48 +0300

    refactor: add fields rect

--- Player.py
+++ Player.py
@@ -8,4 +8,6 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('resources/images/doodle.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 40))
+        self.rectangle = self.image.get_rect()
+        self.rectangle.center = (settings.WIDTH / 2, settings.HEIGHT / 2)
         
