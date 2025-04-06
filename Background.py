ebb6b95f9d98ff4203e612d979b375a9cda66ede
Inferno-kN <aldosovich@bk.ru>
2025-04-06 23:52:38 +0300

    refactor: class Background

--- Background.py
+++ Background.py
@@ -5,3 +5,7 @@ class Background:
     def __init__(self):
         self.image = pygame.Surface((settings.WIDTH, settings.HEIGHT))
         self.image.fill((100, 149, 237))
+        self.rectangle = self.image.get_rect()
+        self.rectangle.topleft = (0, 0)
+        self.speed = 1
+
