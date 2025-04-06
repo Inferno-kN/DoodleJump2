6b15623269b9cf11897272b794badd70b4e5bebc
Inferno-kN <aldosovich@bk.ru>
2025-04-07 00:21:10 +0300

    refactor: add field images

--- Player.py
+++ Player.py
@@ -6,3 +6,6 @@ from Score import Score
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('resources/images/doodle.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30, 40))
+        
