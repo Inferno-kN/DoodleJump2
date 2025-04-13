ae4120924de43a4a672f58c47891a64622b0c85f
Inferno-kN <aldosovich@bk.ru>
2025-04-13 11:21:12 +0300

    refactor: changing def update()

--- src/score.py
+++ src/score.py
@@ -7,7 +7,7 @@ class Score:
         self.font = pygame.font.Font(None, 36)
 
     def update(self):
-        self.score += 50
+        self.score += 1
 
     def draw(self, surface, player_bottom):
         text = self.font.render(f"Текущий счёт: {self.score}", True, BLACK)
