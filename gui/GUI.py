0f76f2e70459856a7e3752b94141bb5069f2df7b
Inferno-kN <aldosovich@bk.ru>
2025-04-14 23:44:21 +0300

    feat: set a grid and place for Label

--- gui/GUI.py
+++ gui/GUI.py
@@ -5,5 +5,7 @@ window = Tk() #коммит
 window.title("The Little Alien") #коммит
 window.geometry('800x600')
 lbl = Label(window, text="Добро пожаловать!", font=("Arial Bold", 20))
+lbl.grid(column=10, row=10)
+lbl.place(x=270, y=30)
 
 
