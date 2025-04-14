a66af852a73795f2fe30863635159d212550413a
Inferno-kN <aldosovich@bk.ru>
2025-04-15 00:05:52 +0300

    feat: create class GUI

--- gui/GUI.py
+++ gui/GUI.py
@@ -1,21 +1,12 @@
 import tkinter
 from tkinter import * #коммит
 
-window = Tk() #коммит
-window.title("The Little Alien") #коммит
-window.geometry('800x600')
-lbl = Label(window, text="Добро пожаловать!", font=("Arial Bold", 20))
-lbl.grid(column=10, row=10)
-lbl.place(x=270, y=30)
+class GUI:
+    def __init__(self, window):
+        self.window = window
+        window.title("The Little Alien")
+        window.geometry('800x600')
 
-btn = tkinter.Button(text='Начать игру')
-btn.place(x=340, y=200)
+        
 
-btn1 = tkinter.Button(text='Посмотреть статистику рекордов')
-btn1.place(x=285, y=260)
-
-btn2 = tkinter.Button(text='Выйти из игры')
-btn2.place(x=335, y=320)
-
-window.mainloop()
 
