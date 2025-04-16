d9d11253a75eb5a2a71baf4e714dfe4d4392cded
Inferno-kN <aldosovich@bk.ru>
2025-04-16 15:00:04 +0300

    feat: added displacement alternation

--- configs/platforms.py
+++ configs/platforms.py
@@ -1,7 +1,6 @@
 import random
-
 from src.platform import Platform
-from configs.settings import  *
+from configs.settings import *
 
 def generate_simple_platform(platform_count: int) -> list[Platform]:
     current_x = WIDTH / 2 - PLATFORM_WIDTH / 2
@@ -11,12 +10,20 @@ def generate_simple_platform(platform_count: int) -> list[Platform]:
     for i in range(platform_count - 1):
         salt = random.randint(-200, 200)
 
+        # Чередование смещения вправо и влево
         if i % 2 == 0:
-            current_x += 100 + salt
+            new_x = current_x + 100 + salt  # Смещение вправо
         else:
-            current_x -= 100 + salt
+            new_x = current_x - 100 - salt  # Смещение влево
+
+        
 
         current_y -= 40
-        platforms.append(Platform(current_x, current_y))
+
+        if current_y < 0:
+            break
+
+        platforms.append(Platform(new_x, current_y))
+        current_x = new_x
 
     return platforms
