f02f6e2363e6e02f74abdfb63588c0e1a019c194
Inferno-kN <aldosovich@bk.ru>
2025-05-26 18:46:56 +0300

    feat: add a new class and update records.json

---
+++ refactoring/source/StorageManager.py
@@ -0,0 +1,21 @@
+from refactoring.cfg.config import records
+import json, os
+
+class Directory:
+    def __init__(self):
+        self.__records = records
+
+    def read(self) -> list[int]:
+        with open(self.__records, "r") as file:
+            if os.path.exists(self.__records) and os.path.getsize(self.__records) > 0:
+                return json.load(file)
+            return []
+
+
+    def write(self, value):
+        record = self.read()
+        if value not in record:
+            with open(self.__records, "w") as file:
+                record.append(value)
+                json.dump(record, file)
+
