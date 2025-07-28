9937555e50958f78f18dd93b03fc4b245832cee1
Inferno-kN <aldosovich@bk.ru>
2025-07-28 15:21:24 +0300

    feat: add a few functions

--- src/InputSystem.py
+++ src/InputSystem.py
@@ -11,10 +11,12 @@ class InputSystem:
         self.__mainloop = mainloop
         self.__doodler = self.__field.get_doodler()
         self.__platforms = self.__field.get_platforms()
+        self.subscriptions = {}
+        self.events = []
+        self.key_states = {}
 
 
     def handle_events(self):
-
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
@@ -31,5 +33,32 @@ class InputSystem:
                 keys = pygame.key.get_pressed()
                 self.__doodler.update_motion(keys, self.__platforms)
 
+
     def get_handle_events(self):
         return self.handle_events()
+
+    def sub(self, key_code, callback):
+        self.subscriptions[key_code] = callback
+
+    def add_event(self, event):
+        self.events.append(event)
+
+    def update(self):
+        for event in self.events:
+            event_type = event.get('type')
+
+            if event_type == 'QUIT':
+                self.quit_game()
+            elif event_type == 'KEYDOWN':
+                key_code = event.get('key')
+                self.key_states[key_code] = True
+                if key_code in self.subscriptions:
+                    self.subscriptions[key_code]()
+            elif event_type == 'KEYUP':
+                key_code = event.get('key')
+                self.key_states[key_code] = False
+
+        self.events.clear()
+
+    def quit_game(self):
+        self.__game.end_game(self.__score.get_score())
